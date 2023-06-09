4b874ca92e73e07eca001bae1f5ef7e9891e841f
Victoria Cavanna <viccavanna@gmail.com>
2023-06-08 23:41:57 -0300

    Ejercicio 4

---
+++ ejercicio4.ts
@@ -0,0 +1,8 @@
+//Imprimir todos los nombres dentro del array, sin importar si se repiten
+let jugadores = ['Pedro', 'Juan', 'Pedro', 'Ignacio', 'Matías', 'Roberto',
+    'Julián', 'Esteban', 'Pedro', 'Alejandro', 'Franco', 'Francisco',
+    'Daniel', 'Pedro', 'Manuel', 'Pedro', 'Marco', 'Javier', 'Miguel'];
+
+jugadores.forEach(function(jugador) {
+    console.log(jugador);
+});
